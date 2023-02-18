3e0c1d94cfb4b5da163b0befed0749f56ffacb1c
Levin-Alexey <levin.penza@yandex.ru>
2023-02-18 20:15:19 +0300

    Готово первое задание

---
+++ 10.1.5.js
@@ -0,0 +1,8 @@
+/*let rubles = prompt('Please, enter your amount:');
+const dollar = 69;
+let rubRate = rubles*dollar;
+alert('The rate is equal to: ' +  rubRate)
+
+console.log(rubRate)*/
+
+console.log('12'<=12)
