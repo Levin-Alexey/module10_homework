69b17061486e849149beeec5ce90a00d56a57e70
Levin-Alexey <levin.penza@yandex.ru>
2023-02-19 13:21:02 +0300

    Решена вторя задача

--- 10.1.5.js
+++ 10.1.5.js
@@ -1,8 +1,11 @@
-/*let rubles = prompt('Please, enter your amount:');
+let rubles = prompt('Please, enter your amount:');
 const dollar = 69;
 let rubRate = rubles*dollar;
 alert('The rate is equal to: ' +  rubRate)
 
-console.log(rubRate)*/
 
-console.log('12'<=12)
+
+
+
+
+
