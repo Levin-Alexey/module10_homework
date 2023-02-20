aa943ebc81e4b8a224b42ad5663e22d55c47aad3
Levin-Alexey <levin.penza@yandex.ru>
2023-02-20 14:44:12 +0300

    Решена задача 6/1.

--- 10.5.5.js
+++ 10.5.5.js
@@ -1,5 +1,28 @@
-let arrAnimals = ['lynx', 'lion', 'panther', 'cheetah', 'leopard'];
+let arrAnimals = ['lynx', 'lion', 'panther', 'cheetah', 'leopard',];
+//Вывод длины массива
 console.log(arrAnimals.length);
+
+//Вывод всех элементов массива
 arrAnimals.forEach(element => console.log(element));
 
+//Сравнение при помощи цикла
+let result;
+for (let i = 0; i < arrAnimals.length - 1; i++) {
+    if (arrAnimals[i] !== arrAnimals[i + 1]) {
+        result = false
+    }
+}
+console.log(result)
+
+
+// сравнение при помощи Set (удаляем повторные элементы и сравниваем массивы)
+let arrAnimalsSet = new Set(arrAnimals)
+
+let arrAnimalsSetToArray = Array.from(arrAnimalsSet);
+
+if (JSON.stringify(arrAnimals) == JSON.stringify(arrAnimalsSetToArray)) {
+    console.log(true)
+} else {
+    console.log(false)
+}
 
